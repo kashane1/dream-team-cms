099965c5036535c1613c9f7c939a8ec5a0ae08d8
Kashane S <ksakhakorn@gmail.com>
2022-03-20 17:09:23 -0700

    getting the structure of the files set up, initial readme and gitignore commit. a little pseudo code prep

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (department_id)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Legal"),
+        ("Human Resources");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Salesperson", "80000", 1),
+        ("Sales Manager", "130000", 1),
+        ("Software Engineer", "120000", 2),
+        ("Lead Engineer", "170000", 2),
+        ("Lawyer", "160000", 3),
+        ("Legal Team Lead", "210000", 3),
+        ("HR Coordinator", "80000", 4),
+        ("HR Manager", "130000", 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Mike", "McKrinkle", 1, 3),
+        ("Sally", "Pickles", 1, 3),
+        ("Jaxon", "Burton", 2, NULL),
+        ("")
---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+// from the readme: You might want to use a separate file that contains functions for performing specific 
+// SQL queries you'll need to use. A constructor function or class could be helpful for organizing these.
+
