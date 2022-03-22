eb524c837ba7e19bb14ae57d8118044dd7c31cc4
Kashane S <ksakhakorn@gmail.com>
2022-03-21 17:27:14 -0700

    started the inquirer prompt and some more pseudo code

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const mysql = require('mysql2');
 // from the readme: You might want to use a separate file that contains functions for performing specific 
 // SQL queries you'll need to use. A constructor function or class could be helpful for organizing these.
 
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+inquirer
+    .prompt([
+        {
+            name: 'start',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+            default: 'view all departments',
+        },
+    ])
+    .then((answers) => {
+        if (answers == 'view all departments') {
+            
+        } else if (answers.confirm_answer) {
+            
+        } else {
+          // the user changed their mind
+          // run the function to ask this question again
+        } 
+      });
